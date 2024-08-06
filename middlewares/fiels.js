0f01a0f4b9cfea7cefd72fed92f46644d9165b49
Hossam El_Deen Kamal <101903513+HossamElDeenKamal2003@users.noreply.github.com>
2024-08-07 01:54:55 +0300

    Add files via upload

--- middlewares/fiels.js
+++ middlewares/fiels.js
@@ -1,22 +1,16 @@
 const multer = require('multer');
-const path = require('path');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require('./cloudinaryConfig');
 
-const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, 'uploads/images/');
-    },
-    filename: function(req, file, cb) {
-        cb(null, `${Date.now()}${path.extname(file.originalname)}`);
-    }
+const storage = new CloudinaryStorage({
+cloudinary: cloudinary,
+params: {
+    folder: 'uploads',
+    format: async (req, file) => 'jpg',
+    public_id: (req, file) => Date.now() + '-' + file.originalname,
+},
 });
 
 const upload = multer({ storage: storage });
 
-const uploadImages = upload.fields([
-    { name: 'licenseImage', maxCount: 1 },
-    { name: 'driver_licence_image', maxCount: 1 }
-]);
-
-module.exports = {
-    uploadImages
-};
+module.exports = upload;
