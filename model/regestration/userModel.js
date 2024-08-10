ed3cbfaefd253516647153eb7231a2d13a382619
Hossam El_Deen Kamal <101903513+HossamElDeenKamal2003@users.noreply.github.com>
2024-08-10 12:31:36 +0300

    Add files via upload

---
+++ model/regestration/userModel.js
@@ -0,0 +1,55 @@
+// models/userModel.js
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    phoneNumber: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    otp: {
+        type: String,
+        default: null
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    }
+});
+
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+if (!this.isModified('password')) {
+    return next();
+}
+try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+} catch (error) {
+    next(error);
+}
+});
+
+// Compare passwords
+userSchema.methods.comparePassword = async function (candidatePassword) {
+return await bcrypt.compare(candidatePassword, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
