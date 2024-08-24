658f1cc6705711cd166d6d385898308f437868a9
Hossam El_Deen Kamal <101903513+HossamElDeenKamal2003@users.noreply.github.com>
2024-08-24 17:49:10 +0300

    Update driverDest.js

--- controller/booking/driverDest.js
+++ controller/booking/driverDest.js
@@ -2,18 +2,20 @@ const mongoose = require('mongoose');
 const destDriver = require('../../model/booking/driversDestination');
 //const io = require('../../server'); 
 
+const { ObjectId } = mongoose.Types;
+
 const updateLocation = async (driverId, longitude, latitude) => {
     if (!driverId || longitude === undefined || latitude === undefined) {
         throw new Error('Driver ID, longitude, and latitude are required');
     }
 
-    if (!mongoose.isValidObjectId(driverId)) {
+    if (!ObjectId.isValid(driverId)) {
         throw new Error('Invalid Driver ID');
     }
 
     try {
         const result = await destDriver.findOneAndUpdate(
-            { _id: driverId },
+            { _id: new ObjectId(driverId) },
             { location: { type: "Point", coordinates: [longitude, latitude] } },
             { new: true }
         );
