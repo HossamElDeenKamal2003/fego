db0dfa94767db400fb21593de31302a314fa27ff
Hossam El_Deen Kamal <101903513+HossamElDeenKamal2003@users.noreply.github.com>
2024-09-01 14:36:27 +0300

    Add files via upload

--- router/userRouter/driverRouter.js
+++ router/userRouter/driverRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { signup, login, updatePassword, patchBlock, DeleteUser, patchAlerts } = require('../../controller/userController/driverController');
+const { signup, login, updatePassword, patchBlock, DeleteUser, patchAlerts, driverLocation } = require('../../controller/userController/driverController');
 const upload = require('../../middlewares/fiels'); 
 
 router.post('/driversignup', upload.fields([{ name: 'licenseImage', maxCount: 1 }, { name: 'driver_licence_image', maxCount: 1 },{ name: 'profile_image', maxCount: 1 }]), signup);
@@ -9,4 +9,5 @@ router.patch('/updatedriverpass', updatePassword);
 router.patch('/patch-block/:id', patchBlock);
 router.delete('/delete-user/:id', DeleteUser);
 router.patch('/patch-alerts:/id', patchAlerts);
+router.get('/location', driverLocation);
 module.exports = router;
