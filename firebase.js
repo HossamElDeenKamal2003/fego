3dc5a65aff4ad1d3c6b991d766bd1f03e9f9961c
Hossam El_Deen Kamal <101903513+HossamElDeenKamal2003@users.noreply.github.com>
2024-09-17 11:36:51 +0300

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,4 +1,3 @@
-// firebase.js
 const admin = require('firebase-admin');
 const serviceAccount = require('./flieger-technology-6a8b2-firebase-adminsdk-fvhxn-0d5c656aaf.json'); // Update this path
 
@@ -6,25 +5,36 @@ admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
 });
 
-const sendNotification = (token, message) => {
+const sendNotification = async (token, message, retries = 3) => {
     const payload = {
         notification: {
             title: message.title,
             body: message.body,
-            //sound: 'default'
         },
         token: token,
     };
 
-    return admin.messaging().send(payload)
-        .then(response => {
-            console.log('Notification sent successfully:', response);
-            return response;
-        })
-        .catch(error => {
-            console.log('Error sending notification:', error);
-            throw error;
-        });
+    try {
+        const response = await admin.messaging().send(payload);
+        console.log('Notification sent successfully:', response);
+        return response;
+    } catch (error) {
+        console.error('Error sending notification:', error);
+
+        // Check if the error is a 503 (Service Unavailable)
+        if (error.code === 'messaging/internal-error' && retries > 0) {
+            console.log(`Retrying... (${3 - retries + 1})`);
+
+            // Wait for a short delay before retrying
+            await new Promise(resolve => setTimeout(resolve, 2000));
+
+            // Retry sending the notification
+            return sendNotification(token, message, retries - 1);
+        }
+
+        // If retries are exhausted or it's not a 503 error, throw the error
+        throw error;
+    }
 };
 
 module.exports = sendNotification;
