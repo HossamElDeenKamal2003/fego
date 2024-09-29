b026bf87e16acefa85c80344ed57bd56eeb57be8
Hossam El_Deen Kamal <101903513+HossamElDeenKamal2003@users.noreply.github.com>
2024-09-29 16:41:54 +0300

    Add files via upload

--- router/userRouter/driverRouter.js
+++ router/userRouter/driverRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { signup, login, updatePassword, patchBlock, handleToken, DeleteUser, patchAlerts, driverLocation } = require('../../controller/userController/driverController');
 const upload = require('../../middlewares/fiels'); 
 
-router.post('/driversignup', upload.fields([{ name: 'licenseImage', maxCount: 1 }, { name: 'driver_licence_image', maxCount: 1 },{ name: 'profile_image', maxCount: 1 }]), signup);
+router.post('/driversignup', upload.fields([{ name: 'licenseImage', maxCount: 1 }, { name: 'driver_licence_image', maxCount: 1 },{ name: 'profile_image', maxCount: 1 }, {name: 'national_front', maxCount: 1}, {name: 'national_back', maxCount: 1}, {name: 'national_selfie', maxCount: 1}]), signup);
 router.post('/driverlogin', login);
 router.patch('/updatedriverpass', updatePassword);
 router.patch('/patch-block/:id', patchBlock);
